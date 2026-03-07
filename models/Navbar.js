0025884f9dc0888345726e44678818ff1515bc4c
Rakesh Singh <sainirakesh3221@gmail.com>
2026-03-07 15:07:18 +0530

    update(backend): schema to support deeply nested dropdowns

--- models/Navbar.js
+++ models/Navbar.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const subItemSchema = new mongoose.Schema({
   id: { type: String, required: true },
   name: { type: String, required: true },
-  path: { type: String, required: true }
+  path: { type: String },
+  type: { type: String, enum: ['link', 'dropdown'], default: 'link' }
+});
+
+subItemSchema.add({
+  dropdown: [subItemSchema]
 });
 
 const navItemSchema = new mongoose.Schema({
@@ -20,8 +25,8 @@ const navbarSchema = new mongoose.Schema({
   version: { type: Number, default: 1 },
   lastUpdated: { type: Date, default: Date.now },
   updatedBy: { type: String, default: 'system' } // You can change this to ObjectId if you have user model
-}, { 
-  timestamps: true 
+}, {
+  timestamps: true
 });
 
 module.exports = mongoose.model('Navbar', navbarSchema);
